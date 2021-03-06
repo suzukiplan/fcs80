c7fc93868e77cd82b2d4e260536e243049644f0c
suzukiplan <suzukiplan.ys@gmail.com>
2021-03-07 01:23:21 +0900

    add scroll feature

--- example/hello/hello.asm
+++ example/hello/hello.asm
@@ -23,17 +23,59 @@ org $0000
     ld de, 394 + $8000 ; 394 = 12 * 32 + 10
     ldir
 
-    ; 無限ループ
-end_loop:
-    jmp end_loop
+    ; メインループ
+mainloop:
+    ; VBLANKを待機
+    call wait_vblank
+
+    ; ジョイパッド(1P)の入力を読み取る
+    ld a, $0E
+    out ($A0), a
+    in a, ($A2)
+    ld b, a
+
+    ; 左カーソルが押されているかチェック（押されている場合は左スクロール）
+    and %00100000
+    jp z, mainloop_check_right
+    ld a, ($9602)
+    inc a
+    ld ($9602), a
+    jmp mainloop_check_up
+mainloop_check_right:
+    ; 右カーソルが押されているかチェック（押されている場合は右スクロール）
+    ld a, b
+    and %00010000
+    jp z, mainloop_check_up
+    ld a, ($9602)
+    dec a
+    ld ($9602), a
+mainloop_check_up:
+    ; 上カーソルが押されているかチェック（押されている場合は上スクロール）
+    ld a, b
+    and %10000000
+    jp z, mainloop_check_down
+    ld a, ($9603)
+    inc a
+    ld ($9603), a
+    jmp mainloop_check_end
+mainloop_check_down:
+    ; 下カーソルが押されているかチェック（押されている場合は下スクロール）
+    ld a, b
+    and %01000000
+    jp z, mainloop_check_end
+    ld a, ($9603)
+    dec a
+    ld ($9603), a
+mainloop_check_end:
+    jmp mainloop
 
 ; VBLANKになるまで待機
 .wait_vblank
-    ld hl, $9600
+    ld hl, $9607
 wait_vblank_loop:
     ld a, (hl)
-    cp $ff
-    jp nz, wait_vblank_loop
+    and $80
+    jp z, wait_vblank_loop
     ret
 
 palette0_data: defw %0000000000000000, %0001110011100111, %0110001100011000, %0111111111111111
